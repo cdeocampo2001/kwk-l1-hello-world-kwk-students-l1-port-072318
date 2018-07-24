d5bab34612e87e82b21495e58b65e76c3c63b902
cdeocampo2001 <sweetcaroline601@gmail.com>
2018-07-24 19:05:38 +0000

    automatically backed up by learn

---
+++ strings.rb
@@ -0,0 +1,8 @@
+puts "welcome to kode with klossy".upcase
+puts "Hello".upcase
+puts "Hello".downcase
+puts "Hello".reverse
+puts "Hello".swapcase
+puts "Hello".capitalize
+puts "Greetings people".length
+puts "Hello"+"from the other side"
